2b94838529b0029be989f2cadbbebf0d5735e1b5
dqpulse2026 <dq_pulse@lsdigital.com>
2026-06-02 12:25:20 +0530

    Switch from BrowserRouter to HashRouter for GitHub Pages compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { AuditProvider } from "./context/AuditContext";
 import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuditProvider>
         <App />
       </AuditProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
