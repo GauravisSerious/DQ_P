b46a4d1a6f807a5621d4c66b261d3131210e3a04
Gaurav Jagtap <gauravjagtap0007@gmail.com>
2026-06-10 21:25:07 +0530

    feat: add Netlify config, redirects, and fix base path for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/DQ_P/',
+  base: '/',
   build: {
-    outDir: 'docs',
+    outDir: 'dist',
   },
   plugins: [react()],
 });
